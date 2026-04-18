54dc9ee29f8917912535ca98f1fccd6fd24a8490
Aryan Kumar <aryanak9163@gmail.com>
2026-04-19 04:43:53 +0530

    feat: implements the moderateFilter in the route

--- src/routes/contact.ts
+++ src/routes/contact.ts
@@ -3,6 +3,7 @@ import type { FastifyReply } from "fastify/types/reply";
 import { contactSchema } from "../schema/Contact";
 import { db } from "../db";
 import { messeges } from "../db/schema";
+import { moderateText } from "../utils/moderation";
 
 export default async function (fastify: FastifyInstance) {
   fastify.post("/contact", async (req: FastifyRequest, reply: FastifyReply) => {
@@ -15,6 +16,16 @@ export default async function (fastify: FastifyInstance) {
 
     const { email, message, name, company } = parsed.data;
     if (company) return reply.send({ success: true }); // bot trap
+
+    const profinityResponse = await moderateText(
+      `${name}\n${email}\n${message}`,
+    );
+
+    if (profinityResponse.flagged) {
+      return reply
+        .code(400)
+        .send({ error: `Message rejected due to policy violation` });
+    }
 
     await db.insert(messeges).values({
       name,
