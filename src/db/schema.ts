a1b1a468feb2fecfcea84e4170bc28ded8ab615a
Aryan Kumar <aryanak9163@gmail.com>
2026-04-18 20:20:39 +0530

    feat: adds the db schema

---
+++ src/db/schema.ts
@@ -0,0 +1,18 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const messeges = pgTable("messages", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  message: text("message").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+export const admins = pgTable("admins", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  email: text("email").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+});
