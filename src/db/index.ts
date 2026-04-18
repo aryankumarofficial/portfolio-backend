20e689242e51196222b9e2268f9e2d3888db6761
Aryan Kumar <aryanak9163@gmail.com>
2026-04-18 20:56:11 +0530

    feat: adds the auth route

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,10 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
 import pg from "pg";
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, {
+  schema,
+});
---
+++ src/routes/auth.ts
@@ -0,0 +1,37 @@
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { db } from "../db";
+import { eq } from "drizzle-orm";
+import { admins } from "../db/schema";
+import { verifyPassword } from "../utils/hash";
+import "@fastify/cookie";
+export default async function (fastify: FastifyInstance) {
+  fastify.post("/login", async (req: FastifyRequest, reply: FastifyReply) => {
+    const { email, password } = req.body as any;
+    const admin = await db.query.admins.findFirst({
+      where: eq(admins.email, email),
+    });
+
+    if (!admin) {
+      return reply.code(401).send({ error: `Invalid Credenticials` });
+    }
+
+    const valid = await verifyPassword(admin.passwordHash, password);
+    if (!valid) {
+      return reply.code(401).send({ error: `Invalid Credenticials` });
+    }
+
+    const token = fastify.jwt.sign({ id: admin.id, email: admin.email });
+
+    reply
+      .setCookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "lax",
+      })
+      .send({ success: true });
+  });
+
+  fastify.post("/logout", async (_, reply) => {
+    reply.clearCookie("token").send({ success: true });
+  });
+}
