fe2973677e5bcd54be0eadc5a9fe6d6c1593ae7d
Aryan Kumar <aryanak9163@gmail.com>
2026-04-19 07:45:33 +0530

    fix: updates the auth prefix

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ app.get("/", async (_, reply) => {
   return reply.send({ message: `Fastify Backend Running` });
 });
 
-app.register(authRoutes, { prefix: "auth" });
+app.register(authRoutes, { prefix: "/auth" });
 app.register(contactRoutes);
 app.register(messageRoutes);
 
